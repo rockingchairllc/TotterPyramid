5eafd2e9dc4f79115b270f62d6e033c06402ba14
root <root@ip-10-245-79-52.ec2.internal>
2011-11-15 03:52:09 +0000

    Initial commit.

---
+++ totter/views.py
@@ -0,0 +1,7 @@
+from totter.models import DBSession
+from totter.models import MyModel
+
+def my_view(request):
+    dbsession = DBSession()
+    root = dbsession.query(MyModel).filter(MyModel.name==u'root').first()
+    return {'root':root, 'project':'totter'}
