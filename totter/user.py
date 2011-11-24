9c0c68d2e750cd0b02da3e53d68b4e4f65fb13e6
Ivan Gudym <igudym@gmail.com>
2011-11-24 20:01:51 +0200

    debug

--- totter/user.py
+++ totter/user.py
@@ -131,7 +131,10 @@ def facebook(request):
 
     # Access denied by user
     elif 'error' in request.params:
-        return HTTPFound(location = request.route_url('login'))
+        return {'message': request.params['error_reason'] + ' ' +
+                           request.params['error'] + ' ' +
+                           request.params['error_description'] }
+#        return HTTPFound(location = request.route_url('login'))
 
     # Call authentication dialog
     fb_url = "https://www.facebook.com/dialog/oauth"
