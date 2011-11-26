c9408d1df0fa540cbf7a0883f3043899c279f85d
Francisco Saldana <frank@rockingchairllc.com>
2011-11-26 01:26:07 -0600

    More testViews template data.

--- totter/testViews.py
+++ totter/testViews.py
@@ -42,7 +42,7 @@ def ideas(request):
     project_id = uuid.UUID(hex=request.matchdict['project_id'])
     session = DBSession()
     project = session.query(Project).filter(Project.id==project_id).one()
-    return {'project' : project, 'user' : user}
+    return {'project' : project, 'user' : user, ideas_count: 1, people_count: 1}
     
 def login(request):
     return {}
