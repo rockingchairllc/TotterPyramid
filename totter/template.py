bd1b72c07df782751ae5216c32109341ac156b8d
Francisco Saldana <frank@rockingchairllc.com>
2011-12-01 23:55:37 -0600

    Jinja2 tojson helper functions.

---
+++ totter/template.py
@@ -0,0 +1,40 @@
+# template.py 
+# Our template helpers
+
+
+
+## Stolen from flask's jinja extensions
+# This is the function we use for the tojinja filter
+
+# try to load the best simplejson implementation available.  If JSON
+# is not installed, we add a failing class.
+json_available = True
+json = None
+try:
+    import simplejson as json
+except ImportError:
+    try:
+        import json
+    except ImportError:
+        try:
+            # Google Appengine offers simplejson via django
+            from django.utils import simplejson as json
+        except ImportError:
+            json_available = False
+
+def _assert_have_json():
+    """Helper function that fails if JSON is unavailable."""
+    if not json_available:
+        raise RuntimeError('simplejson not installed')
+        
+
+# figure out if simplejson escapes slashes.  This behaviour was changed
+# from one version to another without reason.
+if not json_available or '\\/' not in json.dumps('/'):
+
+    def _tojson_filter(*args, **kwargs):
+        if __debug__:
+            _assert_have_json()
+        return json.dumps(*args, **kwargs).replace('/', '\\/')
+else:
+    _tojson_filter = json.dumps
