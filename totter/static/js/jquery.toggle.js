2595497e37fc507ab6984021aacf00c10fecc7b7
Francisco Saldana <frank@rockingchairllc.com>
2011-12-06 22:04:04 -0600

    Repackaged toggle as a jquery library, resolved partial refresh binding issue.

---
+++ totter/static/js/jquery.toggle.js
@@ -0,0 +1,20 @@
+(function( $ ){
+    
+    $.fn.toggle = function( method ) {
+        return this.each(function() {
+            if (method == 'bind') {
+                var $children = $(this).children();
+                $children.each(function() {
+                    $(this).bind('click.toggle', function() { // Child X was clicked.
+                        $children.each(function() { // Show everyone.
+                            $(this).removeClass('hidden');
+                        });
+                        $(this).addClass('hidden'); // Hide X
+                    });
+                });
+            } else if (method == 'unbind') {
+                $.unbind('.toggle');
+            }
+        });
+    };
+})( jQuery );
