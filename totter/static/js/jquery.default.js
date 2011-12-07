3af6d9ceaaf7387b8b6ada11ec7e523ac88c2688
Francisco Saldana <frank@rockingchairllc.com>
2011-12-06 22:06:17 -0600

    Repackaged toggle as a jquery library, resolved partial refresh binding issue.

--- totter/static/js/jquery.default.js
+++ totter/static/js/jquery.default.js
@@ -6,17 +6,20 @@
             // Method calling logic
             if ( method == 'bind') {
                 var def = $this.val();
-                $this.data('default_value', def);
-                $this.click(function() {
+                $this.data('default_value.default', def);
+                $this.bind('click.default', function() {
                     if ($this.val() == def)
                         $this.val('');
                 });
-                $this.blur(function() {
+                $this.bind('blur.default', function() {
                     if ($this.val().trim().length == 0)
                         $this.val(def);
                 });
+            } else if (method == 'unbind') {
+                $('*').unbind('.default');
+                $('*').removeData('.default');
             } else {
-                $this.val($this.data('default_value'));
+                $this.val($this.data('default_value.default'));
             }
         });
     };
