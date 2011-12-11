345ee6883e8b7bce1f1d55d38148fa8cb4814911
Francisco Saldana <frank@rockingchairllc.com>
2011-12-11 01:54:15 -0600

    Remove OAuth2 dep

--- totter/fb.py
+++ totter/fb.py
@@ -2,15 +2,10 @@ import facebook as fb
 from pyramid.security import authenticated_userid
 from pyramid.view import view_config
 
-from facebook import OAuth2
 
 @view_config(route_name='fb')
 def facebook2(request):
     fbuser = fb.get_user_from_cookie(request.cookies, 
-                        request.registry.settings['facebook.app_id'],
-                        request.registry.settings['facebook.secret']
-    )
-    fbuser = OAuth2.getUserFromCookie(request.cookies, 
                         request.registry.settings['facebook.app_id'],
                         request.registry.settings['facebook.secret']
     )
